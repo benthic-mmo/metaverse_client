dc522432ec3019ae7b533f85a6bd048194f1f74e
Skyler Jane Clark <skylerjaneclark@gmail.com>
2021-10-23 14:21:51 -0400

    fixes request -/_ discrepancy, begins mocking out response

--- tests/test_server/test_server.py
+++ tests/test_server/test_server.py
@@ -3,20 +3,68 @@
 
 from xmlrpc.server import SimpleXMLRPCServer 
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+import xml.etree.ElementTree as ET 
+
 PORT = 8000
 HOST = '127.0.0.1'
 
 class RequestHandler(SimpleXMLRPCRequestHandler): 
     rpc_paths = ('/',) 
 
+
+##TODO: generate mock login response'
+## defined here http://opensimulator.org/wiki/SimulatorLoginProtocol 
 with SimpleXMLRPCServer((HOST, PORT), 
         requestHandler = RequestHandler) as server: 
 
-    # this defines the login command.
-    # Doesn't do much right now
-    # just adds to test if it's working
-    def login_to_simulator(x): 
-        return 1
+    def login_to_simulator(xmlData):
+        print(xmlData)
+        xmlData["address_size"]
+        xmlData["agree_to_tos"]
+        xmlData["channel"]
+        xmlData["first"]
+        xmlData["id0"]
+        xmlData["last"]
+        xmlData["last_exec_duration"]
+        xmlData["last_exec_event"]
+        xmlData["mac"]
+        xmlData["options"]
+        xmlData["options"]["adult_compliant"]
+        xmlData["options"]["advanced_mode"]
+        xmlData["options"]["avatar_picker_url"]
+        xmlData["options"]["buddy-list"]
+        xmlData["options"]["classified_categories"]
+        xmlData["options"]["currency"]
+        xmlData["options"]["destination_guide_url"]
+        xmlData["options"]["display_names"]
+        xmlData["options"]["event_categories"]
+        xmlData["options"]["gestures"]
+        xmlData["options"]["global-textures"] 
+        xmlData["options"]["inventory-lib-root"]
+        xmlData["options"]["inventory-root"]
+        xmlData["options"]["inventory-skel-lib"]
+        xmlData["options"]["inventory-skeleton"]
+        xmlData["options"]["login-flags"]
+        xmlData["options"]["map-server-url"]
+        xmlData["options"]["max-agent-groups"]
+        xmlData["options"]["max_groups"]
+        xmlData["options"]["newuser-config"]
+        xmlData["options"]["search"]
+        xmlData["options"]["tutorial_setting"]
+        xmlData["options"]["ui-config"]
+        xmlData["options"]["voice-config"]
+        xmlData["passwd"]
+        xmlData["platform"]
+        xmlData["platform_string"]
+        xmlData["platform_version"]
+        xmlData["read_critical"]
+        xmlData["skipoptional"]
+        xmlData["start"]
+        xmlData["version"]
+        xmlData["viewer_digest"]
+        print(xmlData)
+
+        return 3
     server.register_function(login_to_simulator)
 
     # prints where it's serving on for debug
