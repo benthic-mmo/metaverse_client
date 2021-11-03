8db310833c71f83777ab0e51b8e9ca29f1489621
Skyler Clark <skylerjaneclark@gmail.com>
2021-11-03 19:54:22 -0400

    builds library functions, cleans up tests (#14)

--- tests/login.rs
+++ tests/login.rs
@@ -2,14 +2,17 @@
 
 #[macro_use]
 extern crate lazy_static;
+extern crate sys_info;
 
 use std::collections::HashMap;
+use std::env;
 use std::net::TcpStream;
 use std::panic;
 use std::process::{Child, Command};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
+use login::login::{build_struct_with_defaults, login, login_with_defaults};
 use login::models::simulator_login_protocol::{SimulatorLoginOptions, SimulatorLoginProtocol};
 
 // port and address for the test server
@@ -79,7 +82,7 @@ impl Drop for Reap {
 ///Tests login struct against a dummy python server
 /// verifies that the xml being sent is valid and can be read by other servers
 #[test]
-fn test_struct_validate_xml() {
+fn test_struct_python_validate_xml() {
     let mut reaper = match setup() {
         Ok(reap) => reap,
         Err(_string) => return,
@@ -119,22 +122,16 @@ fn test_struct_osgrid_connect() {
 ///uses your real username and password so be careful not to commit this file !!
 #[test]
 fn test_struct_osgrid_login() {
-    let mut settings = config::Config::default();
-    match settings.merge(config::File::with_name("TestSettings")) {
-        Ok(_file) => _file,
-        Err(..) => {
+    let creds = match read_creds() {
+        Some(x) => x,
+        None => {
             println!("test skipped, no creds file");
             return;
         }
     };
-
-    settings
-        .merge(config::Environment::with_prefix("APP"))
-        .unwrap();
-
-    let creds = settings.try_into::<HashMap<String, String>>().unwrap();
     let creds_firstname = creds.get("first").unwrap().to_string();
     let creds_lastname = creds.get("last").unwrap().to_string();
+
     let auth_login: SimulatorLoginProtocol = SimulatorLoginProtocol {
         first: creds_firstname.clone(),
         last: creds_lastname.clone(),
@@ -154,40 +151,23 @@ fn test_struct_osgrid_login() {
 
     let prod_server_url = build_test_url(OSGRID_URL, OSGRID_PORT);
     let login_response = send_login(auth_login.clone(), prod_server_url.clone());
-    let verify = panic::catch_unwind(|| {
-        assert_eq!(login_response["login"], xmlrpc::Value::from("true"));
-        assert_eq!(
-            login_response["first_name"],
-            xmlrpc::Value::from(creds_firstname)
-        );
-        assert_eq!(
-            login_response["last_name"],
-            xmlrpc::Value::from(creds_lastname)
-        );
-    });
-    if verify.is_err() {
-        assert_eq!(login_response["reason"], xmlrpc::Value::from("presence"))
-    }
+
+    validate_grid_response(login_response, creds_firstname, creds_lastname)
 }
 
 ///Tests the smallest possible request that will facilitate a login on osgrid
 #[test]
 fn test_struct_osgrid_login_minimal() {
-    let mut settings = config::Config::default();
-    match settings.merge(config::File::with_name("TestSettings")) {
-        Ok(_file) => _file,
-        Err(..) => {
+    let creds = match read_creds() {
+        Some(x) => x,
+        None => {
             println!("test skipped, no creds file");
             return;
         }
     };
-    settings
-        .merge(config::Environment::with_prefix("APP"))
-        .unwrap();
-
-    let creds = settings.try_into::<HashMap<String, String>>().unwrap();
     let creds_firstname = creds.get("first").unwrap().to_string();
     let creds_lastname = creds.get("last").unwrap().to_string();
+
     let auth_login: SimulatorLoginProtocol = SimulatorLoginProtocol {
         first: creds_firstname.clone(),
         last: creds_lastname.clone(),
@@ -198,16 +178,157 @@ fn test_struct_osgrid_login_minimal() {
 
     let prod_server_url = build_test_url(OSGRID_URL, OSGRID_PORT);
     let login_response = send_login(auth_login.clone(), prod_server_url.clone());
+
+    validate_grid_response(login_response, creds_firstname, creds_lastname)
+}
+
+///Tests the library login function against python server
+#[test]
+fn test_lib_login_python() {
+    let mut reaper = match setup() {
+        Ok(reap) => reap,
+        Err(_string) => return,
+    };
+
+    match reaper.0.try_wait().unwrap() {
+        None => {}
+        Some(status) => {
+            panic!("python process unexpectedly exited: {}", status);
+        }
+    }
+
+    let test_server_url = build_test_url(PYTHON_URL, PYTHON_PORT);
+    let test_login: SimulatorLoginProtocol = SimulatorLoginProtocol {
+        first: "first".to_string(),
+        last: "last".to_string(),
+        passwd: "passwd".to_string(),
+        start: "last".to_string(),
+        ..SimulatorLoginProtocol::default()
+    };
+
+    let login_response = login(test_login, test_server_url);
+
+    assert_eq!(login_response["first_name"], xmlrpc::Value::from("None"));
+    assert_eq!(login_response["last_name"], xmlrpc::Value::from("None"));
+    assert_eq!(login_response["login"], xmlrpc::Value::from("None"));
+
+    match reaper.0.try_wait().unwrap() {
+        None => {}
+        Some(status) => {
+            panic!("python process unexpectedly exited: {}", status);
+        }
+    }
+}
+
+///Tests connectivity with osgrid, attempts login with invalid credentials using library
+#[test]
+fn test_lib_osgrid_connect() {
+    let prod_server_url = build_test_url(OSGRID_URL, OSGRID_PORT);
+    let login_response = login(EXAMPLE_LOGIN.clone(), prod_server_url);
+    assert_eq!(login_response["login"], xmlrpc::Value::from("false"));
+    assert_eq!(login_response["reason"], xmlrpc::Value::from("key"));
+}
+
+///Tests the smallest possible request that will facilitate a login on osgrid
+#[test]
+fn test_lib_osgrid_login_minimal() {
+    let creds = match read_creds() {
+        Some(x) => x,
+        None => {
+            println!("test skipped, no creds file");
+            return;
+        }
+    };
+    let creds_firstname = creds.get("first").unwrap().to_string();
+    let creds_lastname = creds.get("last").unwrap().to_string();
+
+    let auth_login: SimulatorLoginProtocol = SimulatorLoginProtocol {
+        first: creds_firstname.clone(),
+        last: creds_lastname.clone(),
+        passwd: creds.get("passwd").unwrap().to_string(),
+        start: creds.get("start").unwrap().to_string(),
+        ..SimulatorLoginProtocol::default()
+    };
+
+    let prod_server_url = build_test_url(OSGRID_URL, OSGRID_PORT);
+    let login_response = login(auth_login.clone(), prod_server_url.clone());
+
+    validate_grid_response(login_response, creds_firstname, creds_lastname)
+}
+
+#[test]
+fn test_lib_build_struct_with_defaults() {
+    let default_struct = build_struct_with_defaults(
+        "first".to_string(),
+        "last".to_string(),
+        "passwd".to_string(),
+        "home".to_string(),
+        true,
+        true,
+    );
+
+    assert_eq!(
+        default_struct.platform_string.unwrap(),
+        sys_info::os_release().unwrap()
+    );
+    assert_eq!(
+        default_struct.platform.unwrap(),
+        match env::consts::FAMILY {
+            "mac" => "mac".to_string(),
+            "win" => "win".to_string(),
+            "unix" => "lin".to_string(),
+            _ => "lin".to_string(),
+        }
+    );
+}
+
+///Tests the default login builder
+#[test]
+fn test_lib_osgrid_login_defaults() {
+    let creds = match read_creds() {
+        Some(x) => x,
+        None => {
+            println!("test skipped, no creds file");
+            return;
+        }
+    };
+
+    let creds_firstname = creds.get("first").unwrap().to_string();
+    let creds_lastname = creds.get("last").unwrap().to_string();
+
+    let prod_server_url = build_test_url(OSGRID_URL, OSGRID_PORT);
+    let login_response = login_with_defaults(
+        creds_firstname.clone(),
+        creds_lastname.clone(),
+        creds.get("passwd").unwrap().to_string(),
+        creds.get("start").unwrap().to_string(),
+        true,
+        true,
+        prod_server_url,
+    );
+    validate_grid_response(login_response, creds_firstname, creds_lastname)
+}
+
+fn read_creds() -> Option<HashMap<String, String>> {
+    let mut settings = config::Config::default();
+    match settings.merge(config::File::with_name("TestSettings")) {
+        Ok(_file) => _file,
+        Err(..) => {
+            return None;
+        }
+    };
+    settings
+        .merge(config::Environment::with_prefix("APP"))
+        .unwrap();
+
+    Some(settings.try_into::<HashMap<String, String>>().unwrap())
+}
+
+fn validate_grid_response(login_response: xmlrpc::Value, firstname: String, lastname: String) {
     let verify = panic::catch_unwind(|| {
         assert_eq!(login_response["login"], xmlrpc::Value::from("true"));
-        assert_eq!(
-            login_response["first_name"],
-            xmlrpc::Value::from(creds_firstname)
-        );
-        assert_eq!(
-            login_response["last_name"],
-            xmlrpc::Value::from(creds_lastname)
-        );
+        assert_eq!(login_response["first_name"], xmlrpc::Value::from(firstname));
+        assert_eq!(login_response["last_name"], xmlrpc::Value::from(lastname));
     });
     if verify.is_err() {
         assert_eq!(login_response["reason"], xmlrpc::Value::from("presence"))
