f9e692abb6bfc23eab946c22e146aebe8e76f342
Skyler Clark <skclark@redhat.com>
2024-07-25 12:45:02 -0400

    starts getting session working again, changes uuid dependency

--- crates/session/src/session.rs
+++ crates/session/src/session.rs
@@ -1,56 +1,36 @@
-use crate::models::errors::{LoginError, Reason};
-use crate::models::session_data::Session;
+use metaverse_login::login::{self};
+use metaverse_login::models::login_response::LoginResult;
+use metaverse_login::models::simulator_login_protocol::Login;
 use metaverse_messages::models::use_circuit_code::*;
 
-use std::io;
+use std::{error::Error, io};
 use tokio::net::UdpSocket;
 
-pub fn new_session(login_response: xmlrpc::Value) -> Result<Session, LoginError> {
-    match login_response["login"].as_str().unwrap() {
-        // if login is true then login succeeded
-        "true" => Ok(login_response.into()),
-        // if login is false then login failed
-        "false" => match login_response.get("reason") {
-            // if no reason is sent, something strange happened
-            None => Err(LoginError {
-                reason: Reason::Unknown,
-                message: format!("{:?}", login_response),
-            }),
-            Some(x) => match x.as_str().unwrap() {
-                "key" => Err(LoginError {
-                    reason: Reason::Key,
-                    message: login_response["message"].as_str().unwrap().to_string(),
-                }),
-                "presence" => Err(LoginError {
-                    reason: Reason::Presence,
-                    message: login_response["message"].as_str().unwrap().to_string(),
-                }),
-                _ => Err(LoginError {
-                    reason: Reason::Unknown,
-                    message: login_response["message"].as_str().unwrap().to_string(),
-                }),
-            },
-        },
-        &_ => Err(LoginError {
-            reason: Reason::Unknown,
-            message: "".to_string(),
-        }),
-    }
-}
 
-pub async fn connect(session: Session) -> io::Result<()> {
-    let mut session_addr = session.sim_ip.unwrap().clone();
-    session_addr.push(':');
-    session_addr.push_str(&session.sim_port.unwrap().to_string());
-    let sock = UdpSocket::bind("127.0.0.1:0").await?;
-    sock.connect(session_addr.clone()).await?;
-
+pub async fn new_session(login_data: Login, login_url: String) -> Result<(), Box<dyn Error>>{
+    let login_result = tokio::task::spawn_blocking(|| {
+        login::login(login::build_login(login_data),login_url).unwrap()
+    });
+   
+    let login_response = match login_result.await {
+        Ok(LoginResult::Success(response)) => {
+            response 
+        }
+        Ok(LoginResult::Failure(failure)) => {
+            return Err(format!("Login failed with: {}", failure.message).into());
+        }
+        Err(e) => return Err(format!("Login failed with unknown error").into()),
+    };
+    let mut session_addr = login_response.sim_ip.unwrap().clone();
+    session_addr.push(':'); 
+    session_addr.push_str(&login_response.sim_port.unwrap().to_string()); 
+    let sock = UdpSocket::bind(login_url).await?; 
     let packet = create_use_circuit_code_packet(create_use_circuit_code(
-        session.circuit_code.unwrap(),
-        session.session_id.unwrap(),
-        session.agent_id.unwrap(),
-    ))
-    .unwrap();
-    sock.send(&packet).await?;
+        login_response.circuit_code,
+        login_response.session_id.unwrap(),
+        login_response.agent_id.unwrap()
+    )).unwrap(); 
+
+    sock.send(&packet).await?; 
     Ok(())
 }
--- crates/session/tests/session.rs
+++ crates/session/tests/session.rs
@@ -25,6 +25,13 @@ impl Drop for Reap {
     }
 }
 
+fn init_logger() {
+    let _ = env_logger::builder()
+        .filter(None, LevelFilter::Info)
+        .is_test(true)
+        .try_init();
+}
+
 #[test]
 fn test_mock_session() {
     let mut reaper = match setup() {
@@ -49,7 +56,6 @@ fn test_mock_session() {
         build_test_url(PYTHON_URL, PYTHON_PORT),
     );
 
-    let session = new_session(login_response).unwrap();
     assert_eq!(
         session.home.unwrap().region_handle,
         ("r0".to_string(), "r0".to_string())
@@ -161,132 +167,117 @@ fn test_mock_session() {
 }
 
 #[test]
-fn test_lib_channel() {
-    let defaults_struct = build_struct_with_defaults(
-        env!("CARGO_CRATE_NAME").to_string(),
-        "first".to_string(),
-        "last".to_string(),
-        "passwd".to_string(),
-        "start".to_string(),
-        true,
-        true,
-    );
+async fn test_local(){
+    init_logger();
 
-    assert_eq!(
-        defaults_struct.channel.unwrap(),
-        env!("CARGO_CRATE_NAME").to_string()
-    );
-}
+    let notify = Arc::new(Notify::new());
+    let state = Arc::new(Mutex::new(ServerState::Starting));
 
-#[test]
-fn test_lib_fail_auth_osgrid() {
-    let creds = match read_creds() {
-        Some(x) => x,
-        None => {
-            println!("test skipped, no creds file");
-            return;
-        }
-    };
+    // start the sim server, and initialize logging
+    let sim_server = setup_server(Arc::clone(&notify), Arc::clone(&state)).await;
+    send_setup_commands(&sim_server);
 
-    let login_response = login_with_defaults(
-        env!("CARGO_CRATE_NAME").to_string(),
-        creds.get("osgridfirst").unwrap().to_string(),
-        creds.get("osgridlast").unwrap().to_string(),
-        "incorrectpass".to_string(),
-        creds.get("osgridstart").unwrap().to_string(),
-        true,
-        true,
-        build_test_url(OSGRID_URL, OSGRID_PORT),
-    );
-    let session = new_session(login_response);
-    match session {
-        Ok(v) => assert_eq!(v.login, Some(true)),
-        Err(e) => {
-            assert_eq!(e.reason, Reason::Key)
-        }
+    notify.notified().await;
+    if *state.lock().unwrap() == ServerState::Running {
+        info!("Server started. Running test commands");
+        sim_server.do_send(CommandMessage{
+            command: "create user default user password email@email.com 9dc18bb1-044f-4c68-906b-2cb608b2e197 default".to_string()
+        });
+
+        tokio::task::spawn_blocking(|| {
+            let session = new_session(
+                Login{first: "default".to_string(), 
+                    last:"user".to_string(),
+                passwd: "password".to_string(),
+                start: "home".to_string(),
+                channel: "benthic".to_string(),
+                agree_to_tos: true, 
+                read_critical: true}, build_test_url("http://127.0.0.1", 9000)
+            );
+
+        });
+        sleep(Duration::from_secs(5)).await;
+        sim_server.do_send(CommandMessage {
+            command: "quit".to_string(),
+        });
+    } else {
+        panic!("server failed to start")
     }
+
+    notify.notified().await;
+
+    new_session(login_data, login_url)
+
 }
 
-#[test]
-fn test_lib_auth_osgrid() {
-    let creds = match read_creds() {
-        Some(x) => x,
-        None => {
-            println!("test skipped, no creds file");
-            return;
-        }
+
+fn send_setup_commands(sim_server: &actix::Addr<SimServer>) {
+    // This is required for first time startup. This assigns the default user as the region owner.
+    // TODO: make this into a sql query that automatically adds this to the default region on
+    // startup
+    sim_server.do_send(CommandMessage {
+        command: "default".to_string(),
+    });
+    sim_server.do_send(CommandMessage {
+        command: "user".to_string(),
+    });
+    sim_server.do_send(CommandMessage {
+        command: "password".to_string(),
+    });
+    sim_server.do_send(CommandMessage {
+        command: "email@email.com".to_string(),
+    });
+    sim_server.do_send(CommandMessage {
+        command: "9dc18bb1-044f-4c68-906b-2cb608b2e197".to_string(),
+    });
+}
+
+async fn setup_server(
+    notify: Arc<Notify>,
+    state: Arc<Mutex<ServerState>>,
+) -> actix::Addr<SimServer> {
+    let (stdin_sender, stdin_receiver) = mpsc::channel::<CommandMessage>(100);
+    let (stdout_sender, mut receiver) = mpsc::channel::<StdoutMessage>(100);
+
+    let (url, archive, base_dir, executable) = match read_config() {
+        Ok((url, archive, base_dir, executable)) => (url, archive, base_dir, executable),
+        Err(e) => panic!("Error: {}", e),
     };
 
-    let login_response = login_with_defaults(
-        env!("CARGO_CRATE_NAME").to_string(),
-        creds.get("osgridfirst").unwrap().to_string(),
-        creds.get("osgridlast").unwrap().to_string(),
-        creds.get("osgridpasswd").unwrap().to_string(),
-        creds.get("osgridstart").unwrap().to_string(),
-        true,
-        true,
-        build_test_url(OSGRID_URL, OSGRID_PORT),
-    );
-    let session = new_session(login_response);
-    match session {
-        Ok(v) => assert_eq!(v.login, Some(true)),
-        Err(e) => {
-            assert_eq!(e.reason, Reason::Presence)
-        }
+    info!("downloading server. On first run, this may take a while");
+    match download_sim(&url, &archive, &base_dir).await {
+        Ok(_) => info!("downloaded sim successfully"),
+        Err(e) => info!("failed to download sim {}", e),
+    };
+
+    let sim_server = SimServer {
+        state: Arc::clone(&state),
+        sim_config: create_default_config(),
+        standalone_config: create_default_standalone_config(),
+        regions_config: create_default_region_config(),
+        process: None,
+        process_stdout_sender: Some(stdout_sender),
+        process_stdin_receiver: Some(stdin_receiver),
+        process_stdin_sender: Some(stdin_sender),
+        notify: Arc::clone(&notify),
+        exec_data: ExecData {
+            base_dir,
+            executable,
+            init_command: "mono".to_string(),
+        },
     }
-}
+    .start();
+    info!("Waiting for the server to start...");
 
-#[test]
-fn test_lib_auth_3rdrock() {
-    let creds = match read_creds() {
-        Some(x) => x,
-        None => {
-            println!("test skipped, no creds file");
-            return;
+    tokio::spawn(async move {
+        loop {
+            if let Some(msg) = receiver.recv().await {
+                info!("Received message: {}", msg.log_content);
+                if msg.log_content.contains("Currently selected region is") {}
+            }
         }
-    };
-
-    let login_response = login_with_defaults(
-        env!("CARGO_CRATE_NAME").to_string(),
-        creds.get("3rdrockfirst").unwrap().to_string(),
-        creds.get("3rdrocklast").unwrap().to_string(),
-        creds.get("3rdrockpasswd").unwrap().to_string(),
-        creds.get("3rdrockstart").unwrap().to_string(),
-        true,
-        true,
-        build_test_url(THIRDROCK_URL, THIRDROCK_PORT),
-    );
-    let session = new_session(login_response);
-    match session {
-        Ok(v) => assert_eq!(v.login, Some(true)),
-        Err(e) => {
-            assert_eq!(e.reason, Reason::Presence)
-        }
-    }
-}
-
-#[test]
-fn establish_conn() {
-    let creds = match read_creds() {
-        Some(x) => x,
-        None => {
-            println!("test skipped, no creds file");
-            return;
-        }
-    };
-
-    let login_response = login_with_defaults(
-        env!("CARGO_CRATE_NAME").to_string(),
-        creds.get("osgridfirst").unwrap().to_string(),
-        creds.get("osgridlast").unwrap().to_string(),
-        creds.get("osgridpasswd").unwrap().to_string(),
-        creds.get("osgridstart").unwrap().to_string(),
-        true,
-        true,
-        build_test_url(OSGRID_URL, OSGRID_PORT),
-    );
-    let session = new_session(login_response);
-    tokio_test::block_on(connect(session.unwrap())).unwrap();
+    });
+    sim_server
 }
 
 fn read_creds() -> Option<HashMap<String, String>> {
