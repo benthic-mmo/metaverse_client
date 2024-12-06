8559beac01e354fa9c048a819d7dc1d7f450a516
Skyler Clark <skclark@redhat.com>
2024-12-06 16:52:39 -0500

    moves to bevy_egui
    This is a much more functional option. Honestly this should be part of
    built in bevy. This rules.
    Now that I have my login working properly, I can use the login button to
    trigger the initializaion of the session normally. The current setup
    does NOT work, because the login data is empty since the actix session
    starter starts up at startup when that struct is empty

--- crates/ui/src/login.rs
+++ crates/ui/src/login.rs
@@ -1,126 +1,103 @@
-use std::{error::Error, sync::{Arc, Mutex}};
+use std::{
+    error::Error,
+    sync::{Arc, Mutex},
+};
 
 use crate::utils::Notification;
 
-use bevy::{color::palettes::basic::*, prelude::*};
+use bevy::prelude::*;
 use metaverse_login::models::simulator_login_protocol::Login;
-use metaverse_messages::models::{chat_from_viewer::{ChatFromViewer, ClientChatType}, client_update_data::ClientUpdateData, packet::Packet};
+use metaverse_messages::models::{
+    chat_from_viewer::{ChatFromViewer, ClientChatType},
+    client_update_data::ClientUpdateData,
+    packet::Packet,
+};
 use metaverse_session::session::Session;
-use uuid::Uuid;
-const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+use bevy_egui::{egui, EguiContexts};
 
 #[derive(Event)]
 pub struct ClientEvent(pub Packet);
 
-pub fn setup_login_ui(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>
-){
-    commands.spawn(Camera2dBundle::default());
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                ..default()
-            },
-            ..default()
-        })
-        .with_children(|parent| {
-            parent
-                .spawn(ButtonBundle {
-                    style: Style {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    border_color: BorderColor(Color::BLACK),
-                    border_radius: BorderRadius::MAX,
-                    background_color: NORMAL_BUTTON.into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent.spawn(TextBundle::from_section(
-                        "Button",
-                        TextStyle {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 40.0,
-                            color: Color::srgb(0.9, 0.9, 0.9),
-                        },
-                    ));
-                });
-        });
+#[derive(Default, Resource, Clone)]
+pub struct LoginState {
+    first_name: String,
+    last_name: String,
+    password: String, 
+    grid: String,
+    is_window_open: bool,
 }
 
-pub fn button_system(
-    mut interaction_query: Query<
-        (
-            &Interaction,
-            &mut BackgroundColor,
-            &mut BorderColor,
-            &Children,
-        ),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
-    notify: Res<Notification>,
-    mut ev_press: EventWriter<ClientEvent>,
-) {
-    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Pressed => {
-                text.sections[0].value = "Press".to_string();
-                *color = PRESSED_BUTTON.into();
-                border_color.0 = RED.into();
-                // login button was pressed, notify the actix thread to login
-                notify.0.notify_one();
+pub fn configure_ui_state_system(mut ui_state: ResMut<LoginState>) {
+    ui_state.is_window_open = true;
+}
 
-                ev_press.send(ClientEvent(Packet::new_chat_from_viewer(ChatFromViewer {
-                    agent_id: Uuid::nil(),   // set these to nil and set them properly later
-                    session_id: Uuid::nil(), // set these to nil and set them properly later
-                    message: "hello world".to_string(),
-                    message_type: ClientChatType::Normal,
-                    channel: 0,
-                })));
-                println!("pressed");
-            }
-            Interaction::Hovered => {
-                text.sections[0].value = "Hover".to_string();
-                *color = HOVERED_BUTTON.into();
-                border_color.0 = Color::WHITE;
-            }
-            Interaction::None => {
-                text.sections[0].value = "Button".to_string();
-                *color = NORMAL_BUTTON.into();
-                border_color.0 = Color::BLACK;
-            }
-        }
+pub fn ui_example_system(
+    mut login_state: ResMut<LoginState>,
+    mut is_initialized: Local<bool>,
+    mut contexts: EguiContexts,
+    notify: Res<Notification>,
+) {
+    if !*is_initialized {
+        *is_initialized = true;
+    }
+
+    let mut login = false;
+    let ctx = contexts.ctx_mut();
+
+    egui::SidePanel::left("side_panel")
+        .default_width(200.0)
+        .show(ctx, |ui| {
+            ui.heading("Side Panel");
+
+            ui.horizontal(|ui| {
+                ui.label("first name: ");
+                ui.text_edit_singleline(&mut login_state.first_name);
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("last name: ");
+                ui.text_edit_singleline(&mut login_state.last_name);
+            });
+            ui.horizontal(|ui| {
+                ui.label("Password: ");
+                ui.text_edit_singleline(&mut login_state.password);
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("Grid: ");
+                ui.text_edit_singleline(&mut login_state.grid);
+            });
+            
+            ui.allocate_space(egui::Vec2::new(1.0, 100.0));
+            ui.horizontal(|ui| {
+                login = ui.button("Login").clicked();
+            });
+        });
+
+    if login {
+        notify.0.notify_one();
     }
 }
 
-pub async fn create_session(stream: Arc<Mutex<Vec<ClientUpdateData>>>) -> Result<Session, Box<dyn Error>> {
-    let (first_name, last_name, password, grid) = get_user_login();
+pub async fn create_session<'a>(
+    stream: Arc<Mutex<Vec<ClientUpdateData>>>,
+    login_state: LoginState,
+) -> Result<Session, Box<dyn Error>> {
+    println!("FIRST NAME {}", login_state.first_name);
+    println!("LAST NAME {}", login_state.last_name);
+    println!("GRID {}", login_state.grid);
+    println!("PASSWORD {}", login_state.password);
     let result = Session::new(
-        Login {            
-            first: first_name.to_string(),
-            last: last_name.to_string(),
-            passwd: password.to_string(),
+        Login {
+            first: login_state.first_name,
+            last: login_state.last_name,
+            passwd: login_state.password,
             channel: "benthic".to_string(),
             start: "home".to_string(),
             agree_to_tos: true,
             read_critical: true,
         },
-        grid,
+        build_url("http://127.0.0.1", 9000),
         stream.clone(),
     )
     .await;
@@ -128,20 +105,6 @@ pub async fn create_session(stream: Arc<Mutex<Vec<ClientUpdateData>>>) -> Result
         Ok(s) => Ok(s),
         Err(e) => Err(Box::new(e)),
     }
-}
-
-
-fn get_user_login() -> (String, String, String, String) {
-    let first_name = "default";
-    let last_name = "user";
-    let password = "password";
-    let grid = build_url(&"http://127.0.0.1", 9000);
-    return (
-        first_name.to_string(),
-        last_name.to_string(),
-        password.to_string(),
-        grid.to_string(),
-    );
 }
 
 fn build_url(url: &str, port: u16) -> String {
@@ -152,4 +115,3 @@ fn build_url(url: &str, port: u16) -> String {
     println!("url string {}", url_string);
     url_string
 }
-
