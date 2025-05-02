b2e61fc516348745a17da49f0439a2e6578383ad
Skyler Clark <skclark@redhat.com>
2025-05-02 19:49:26 -0400

    Gets rendering working

---
+++ crates/ui/src/environment.rs
@@ -0,0 +1,70 @@
+use bevy::prelude::*;
+use metaverse_messages::ui::custom::layer_update::LayerUpdate;
+
+#[derive(Event)]
+pub struct LayerUpdateEvent {
+    pub value: LayerUpdate,
+}
+
+#[derive(Resource)]
+pub struct PendingLayer {
+    pub handle: Handle<Gltf>,
+    pub position: Vec3,
+}
+
+pub fn setup_environment(
+    mut commands: Commands,
+) {
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
+
+
+pub fn handle_layer_update(
+    mut ev_layer_update: EventReader<LayerUpdateEvent>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    let factor = 0;
+    for layer_update in ev_layer_update.read() {
+        let x = layer_update.value.position.x * factor;
+        let y = layer_update.value.position.y * factor;
+        let handle: Handle<Gltf> = asset_server.load(layer_update.value.path.clone());
+        commands.insert_resource(PendingLayer {
+            handle,
+            position: Vec3::new(x as f32, y as f32, 0.0),
+        });
+    }
+}
+
+pub fn check_model_loaded(
+    mut commands: Commands,
+    pending: Option<Res<PendingLayer>>,
+    layer_assets: Res<Assets<Gltf>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    if let Some(pending_layer) = pending {
+        if let Some(gltf) = layer_assets.get(&pending_layer.handle) {
+            println!("spawning gltf");
+            let white_material = materials.add(StandardMaterial {
+                base_color: Color::WHITE,
+                ..Default::default()
+            });
+            commands.spawn((
+                SceneRoot(gltf.scenes[0].clone()), 
+                Transform::from_translation(pending_layer.position),
+                MeshMaterial3d::from(white_material.clone())
+            ));
+            commands.remove_resource::<PendingLayer>();
+        }
+    }
+}
