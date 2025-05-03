da4d7db175e8126f083950159f10e0aca6a5ff69
Skyler Clark <skclark@redhat.com>
2025-05-02 21:15:31 -0400

    upgrade to bevy 16.0

--- crates/ui/src/chat.rs
+++ crates/ui/src/chat.rs
@@ -1,6 +1,7 @@
 use std::net::UdpSocket;
 
-use bevy::ecs::system::{Res, ResMut, Resource};
+use bevy::ecs::system::{Res, ResMut};
+use bevy::prelude::Resource;
 use bevy_egui::{EguiContexts, egui};
 use metaverse_messages::{
     chat_from_viewer::{ChatFromViewer, ClientChatType},
--- crates/ui/src/environment.rs
+++ crates/ui/src/environment.rs
@@ -24,7 +24,7 @@ pub fn setup_environment(
     ));
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0.0, 0.0, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 }
 
